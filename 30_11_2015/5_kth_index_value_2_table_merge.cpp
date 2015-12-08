7cb6c81198090db0b5377bb8e52c63f6d7bd326c
chmanoj133 <chitrada.manoj@gmail.com>
2015-12-08 20:31:10 +0530

    Done

--- 30_11_2015/5_kth_index_value_2_table_merge.cpp
+++ 30_11_2015/5_kth_index_value_2_table_merge.cpp
@@ -1,3 +1,9 @@
+/*
+5.Given three numbers n1,n2,and r. Merge the table of n1 and n2 and find the r th number. In merging it should eliminate duplicate values (tables consists the multiples of those numbers ,ie 3 contains 3,6,9,12..)
+Ex , n1=3 ,n2=5 and r=5, The merged table is 3,5,9,10,12,15(One more 15 is removed as its duplicate ) 18,20,21,24,25........so on .
+The rth num ie 5th number is 12 ,so OP is 12
+*/
+
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
